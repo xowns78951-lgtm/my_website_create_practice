27261cfcc5bbe19f5db312267eb714953fb05fc8
xowns78951-lgtm <xowns78951@gmail.com>
2026-01-12 01:52:48 +0900

    4-5 관리자 계정 로그인,JWT 토큰

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt")
 const User = require("../models/User")
+const axios = require("axios")
+const jwt = require("jsonwebtoken")
 
 router.post("/signup", async (req, res) => {
   try {
@@ -24,5 +26,88 @@ router.post("/signup", async (req, res) => {
     res.status(500).json({message:"서버 오류가 발생했습니다."});
   }
 });
+
+router.post('/login', async(req, res) => {
+  try {
+    const{ username, password } = req.body;
+
+    const user = await User.findOne({ username }).select('+password');
+
+    if(!user) {
+      return res.status("401").json({ message: "사용자를 찾을 수 없습니다."})
+    };
+
+    if(!user.isActive){
+      return res
+        .status(401)
+        .json({ message: "비활성화된 계정입니다. 관리자에게 문의하세요"})
+    };
+
+    if(user.isLoggedIn) {
+      return res
+        .status(401)
+        .json({message:"이미 다른 기기에서 로그인이되어 있습니다."})
+    };
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if(!isValidPassword){
+      user.faiiedLoginAttempts += 1;
+      user.lastLoginAttempt= new Date()
+
+      if (user.faiiedLoginAttempts >= 5){
+        user.isActive=false;
+        await user.save();
+        return res.status(401).json({message:"비밀번호를 5회 이상 틀려 계정이 비활성화 되었습니다."})
+      };
+
+      await user.save();
+      return res
+        .status(401)
+        .json(
+              { 
+                message : "비밀번호가 일치하지 않습니다.", 
+                remainingAttemps : 5 - user.faiiedLoginAttempts
+              }
+            );
+    };
+
+    user.faiiedLoginAttempts = 0;
+    user.lastLoginAttempt = new Date();
+    user.isLoggedIn = true;
+
+    try {
+      const response = await axios.get("https://api.ipify.org?format=json");
+      const ipAddress = response.data.ip;
+      user.ipAddress = ipAddress;
+    } catch(error){
+      console.log("IP 주소를 가져오던 중 오류 발생:", error.message)
+    };
+
+    await user.save();
+
+    const token = jwt.sign(
+      { userId : user._id, username:user.username },
+      process.env.JWT_SECRET,
+      { expiresIn : "24h" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly:true,
+      secure: "production",
+      sameste : "strict",
+      maxAge : 24 * 60 * 60 * 1000,
+    });
+
+    const userWithoutPassword = user.toObject();
+    delete userWithoutPassword.password;
+
+    res.json({ user: userWithoutPassword });
+
+  }catch(error){
+    console.log("서버 오류: ", error.message);
+    res.status(500).json({ message : "서버 오류가 발생 했습니다." })
+  };
+})
 
 module.exports=router;
