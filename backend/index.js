7a9bbc639a373b2129cc2360a5bc2ecb18d2b0ed
xowns78951-lgtm <xowns78951@gmail.com>
2026-01-12 01:03:30 +0900

    ch4-4 관리자 계정 생성하기

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,20 @@ const mongoose = require("mongoose")
 const app = express();
 const PORT = 3000;
 
+const userRoutes = require("./routes/user");
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.use("/api/auth", userRoutes);
+
 app.get("/", (req,res)=>{
     res.send("Hello World");
 });
 
 mongoose
     .connect(process.env.MONGO_URI)
-    .then(()=> console.log("MongoDB와 연길이 되었습니다."))
+    .then(()=> console.log("MongoDB와 연결이 되었습니다."))
     .catch((error)=>console.log("MongoDB와 연결에 실패했습니다: ", error));
 
 app.listen(PORT,()=>{
