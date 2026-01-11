9f943ef099aba6d0f0a62dddd29cc0f606f42214
xowns78951-lgtm <xowns78951@gmail.com>
2026-01-12 00:10:36 +0900

    ch4-3 MongoDB와 Express 서버연동

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express = require('express');
+const mongoose = require("mongoose")
 const app = express();
 const PORT = 3000;
 
@@ -6,6 +8,11 @@ app.get("/", (req,res)=>{
     res.send("Hello World");
 });
 
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(()=> console.log("MongoDB와 연길이 되었습니다."))
+    .catch((error)=>console.log("MongoDB와 연결에 실패했습니다: ", error));
+
 app.listen(PORT,()=>{
-    console.log("Server is runing")
-})
+    console.log("Server is runing");
+});
